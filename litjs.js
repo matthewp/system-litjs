38bc7f37f8d0bc59902e4f2dfd0b76a441e9400a
Matthew Phillips <matthew@matthewphillips.info>
2014-10-23 19:59:34 -0400

    Working

--- litjs.js
+++ litjs.js
@@ -1,17 +1,16 @@
-var processComment = require('documentjs/process/comment');
-var tags = require('documentjs/tags/tags');
+var markedModuleName = __dirname.indexOf('bower_components')
+  ? 'bower_components/marked/lib/marked' : 'marked';
 
 exports.translate = function(load) {
-  var source = load.source;
-  debugger;
+  return this.import(markedModuleName).then(function(marked) {
+    var parsed = marked.lexer(load.source);
+    var source = '';
 
-  return new Promise(function(resolve, reject) {
-    debugger;
-    processComment({
-      comment: load.source,
-      tags: tags
-    }, function(props, scope) {
-      debugger;
+    parsed.forEach(function(item) {
+      if(item.type === 'code') {
+        source += item.text + '\n';
+      }
     });
+    load.source = source;
   });
 };
